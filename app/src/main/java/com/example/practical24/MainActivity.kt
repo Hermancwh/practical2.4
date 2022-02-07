6c4b616429c1ee43a920427b8a68177b29b28e55
Hermancwh <chanwh-wm19@student.tarc.edu.my>
2022-02-07 21:31:11 +0800

    added binding

---
+++ app/src/main/java/com/example/practical24/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.practical24
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.practical24.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val s: Student = Student("W123", "John")
+
+        binding.myData = s
+        //binding.tvID.text = s.studentID
+
+        binding.btnUpdate.setOnClickListener(){
+            s.studentName = "Chong"
+
+            binding.apply {
+                invalidateAll()
+            }
+        }
+    }
+}
